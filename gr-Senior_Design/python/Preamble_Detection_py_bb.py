683a033e92e66ac4fb39c7e0565f54232763681d
William Lahann <william.lahann@gmail.com>
2018-03-19 15:41:48 -0400

    Corrected error when consumming input data.
    Error still present that prevents output of small portions of data.
    Cause unknown.

--- gr-Senior_Design/python/Preamble_Detection_py_bb.py
+++ gr-Senior_Design/python/Preamble_Detection_py_bb.py
@@ -50,8 +50,9 @@ class Preamble_Detection_py_bb(gr.basic_block):
                                 name="Preamble_Detection_py_bb",
                                 in_sig=[numpy.byte],
                                 out_sig=[numpy.byte])
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         print("Getting started. Preamble is set to: " + str(self.preamble))
-        print("Version 3.18.18")
+        print("Version 3.19.18...")
 
     def forecast(self, noutput_items, ninput_items_required):
         # setup size of input_items[i] for work call
@@ -69,21 +70,26 @@ class Preamble_Detection_py_bb(gr.basic_block):
     def general_work(self, input_items, output_items):
         count = -1  # Count set to -1 because each for loop initiates at current count +1
         data_to_output = 0
-        if self.DebugPrints >= 1:
+        if self.DebugPrints >= 2:
             print("General: Startup: Input Items Length = " + str(len(input_items[0])))
         while count < len(input_items[0])-1:
             # Search for the start of the packet. This will aline the bits.
             if self.state == self.SEARCH:
                 # If preamble is found. Consume everything prior.
                 # Only raise a flag, do not output anything yet.
-                if self.DebugPrints >= 0:
+                if self.DebugPrints >= 1:
                     print("General: Searching: Starting.")
                 for count in range(count + 1, len(input_items[0])):
-                    print("General: Searching: Count = " + str(count) + ", Value = " + str(input_items[0][count]))
+                    if self.DebugPrints >= 4:
+                        print("General: Searching: Inside For Loop. Count = " + str(count))
+                    if self.DebugPrints >= 4:
+                        print("General: Searching: Count = " + str(count) + ", Value = " + str(input_items[0][count]))
                     self.preamblequeue = (
                                                      self.preamblequeue << 1) & 0xFFFFFFFF  # shift then mask to ensure that we stay at 32 bits
                     self.preamblequeue = self.preamblequeue | (
                                 0x01 & input_items[0][count])  # Set the LSB of input to LSB of queue
+                    if self.DebugPrints >= 3:
+                        print("General: Searching: PreambleQueue: " + str(self.preamblequeue))
                     if self.preamblequeue == self.preamble:
                         if self.DebugPrints >= 0:
                             print("General: Searching: Found preamble: " + str(self.preamblequeue))
@@ -95,7 +101,7 @@ class Preamble_Detection_py_bb(gr.basic_block):
             # Parse through the header of the packet to determine it's properties.
             elif self.state == self.HEADER:
                 # This should be the first state entered after the preamble was found.
-                if self.DebugPrints >= 0:
+                if self.DebugPrints >= 1:
                     print("General: Header: Starting.")
                 for count in range(count + 1, len(input_items[0])):
                     self.bitqueue[0] = (self.bitqueue[0] & (
@@ -107,14 +113,17 @@ class Preamble_Detection_py_bb(gr.basic_block):
                         self.bitcounter = 7  # Reset the bitcounter for the next byte.
                         self.headerqueue.append(self.bitqueue[0])
                     if len(self.headerqueue) == self.headersize:
-                        # print("Parsing Header!")
+                        if self.DebugPrints >= 1:
+                            print("General: Header: Parsing Header!")
                         # Reset header values.
                         self.packet_length = 0
                         for i in range(len(self.headerqueue)):
+                            if self.DebugPrints >= 5:
+                                print("General: Header: Inside For Loop. Count = " + str(count))
                             if i < 4:  # This is the first section of the header, the packet length
                                 self.packet_length += self.headerqueue[i] * numpy.power(10, (3 - i))
                         if self.packet_length > 10000:
-                            if self.DebugPrints >=0:
+                            if self.DebugPrints >=1:
                                 print("General: Header: Packet length " + str(
                                     self.packet_length) + " is too large. Assuming noise, restarting search.")
                             self.state = self.SEARCH
@@ -126,9 +135,11 @@ class Preamble_Detection_py_bb(gr.basic_block):
 
             # Pass packet data to the output of the block.
             elif self.state == self.RUNNING:
-                if self.DebugPrints >= 0:
+                if self.DebugPrints >= 2:
                     print("General: Running: Started.")
                 for count in range(count + 1, len(input_items[0])):
+                    if self.DebugPrints >= 5:
+                        print("General: Running: Inside For Loop. Count = " + str(count))
                     self.bitqueue[0] = (self.bitqueue[0] & (
                                 0xFE << self.bitcounter)) & 0xFF  # Set the bit we are about to record to 0.
                     self.bitqueue[0] = (self.bitqueue[0] | (
@@ -146,7 +157,7 @@ class Preamble_Detection_py_bb(gr.basic_block):
                         # It is not only a counter.
                         if self.received_length == self.packet_length:  # We've received the whole packet.
                             # Lower the flags.
-                            if self.DebugPrints>=0:
+                            if self.DebugPrints>=2:
                                 print("General: Running: Entire packet was received! Resetting state.")
                             self.state = self.SEARCH  # Start looking for the start of the next packet.
                             self.received_length = 0
@@ -154,11 +165,16 @@ class Preamble_Detection_py_bb(gr.basic_block):
             else:
                 print("General: No State: Something happened.")
                 self.state = self.SEARCH
-            print("General: Looping!")
+            if self.DebugPrints >= 3:
+                print("General: Finished Loop.")
 
         # self.consume_each(consumable)
-        if self.DebugPrints:
-            print("General: Leaving: Consuming = " + str(count))
+        if self.DebugPrints >= 1:
+            print("General: Leaving: Consuming = " + str(count + 1))
             print("General: Leaving: Outputting = " + str(data_to_output))
-        self.consume(0, count)
+            if data_to_output != 0:
+                print("General: Leaving: DATA OUTPUTTED!")
+                print(output_items[0][0:data_to_output])
+        self.consume(0, count+1) # Count is used as an index and starts at 0, must consume 0th input though.
+                                 # Consume is done using counting numbers, so index 0-4 would be 5 things consumed
         return data_to_output
