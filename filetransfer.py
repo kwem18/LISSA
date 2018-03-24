4abc94b93f507045ecc6f239db64d64447c4b3be
kwem18 <37224053+kwem18@users.noreply.github.com>
2018-03-24 15:51:28 -0400

    added dummy pkts into the pyld
    3 dummy packts added before and after relevant payload data.

--- filetransfer.py
+++ filetransfer.py
@@ -12,6 +12,7 @@ import sys,os
 import fileManip 		#import split/comb def...Remember that this file must be in same diectory as sender.py!!
 import time
 import numpy as np
+from shutil import rmtree as delete
 #steps:
 #1. get jpeg and split file into parts
 #2. take each segment and add preamble,length, and name of segment
@@ -24,8 +25,10 @@ import numpy as np
 ##Take  test jpeg file in directory and split it using fileManip definitions
 intial = time.clock()
 
+os.makedirs("INPUT") # Create the input folder to house the file partitions while code is running.
+
 orig_file = 'sat_pic.JPG' #grab file name from cmd input
-prefix = 'pkt'			#Using string 'pic' for header later
+prefix = 'INPUT/pkt'			#Using string 'pic' for header later
 preamble = 26530			#preamble for 'skipheader' gnuradio block
 fsize = 1000			#Size of segments we want for split def (in bytes)
 
@@ -37,8 +40,8 @@ final = ''			#final variable will hold compilationf of msg pylds that will have 
 file_list = []		#This list will contain the filesnames of file segments
 BER_CHECK = ''
 print "----------------------------------------\nBEGIN FILE_LIST APPEND\n----------------------------------------"
-for found in glob.glob("pkt*"):
-	#found = found.split('/')[1]	#create Nx2 list by seperating file name from prefix (OUTDATED)
+for found in glob.glob(prefix+"*"):
+	found = found.split('/')[1]	#create Nx2 list by seperating file name from prefix (OUTDATED)
 	#print "found=:",found		#check whats the string in question
 	file_list.append(found)		#add string into file_list[]
 file_list.sort()
@@ -47,8 +50,9 @@ print "file_list:",file_list				#PRINT file_list(debug)
 
 
 lenfieldsize =4 					#length added after filename will be four bytes exactly
+new = packetize(preamble = preamble)					#create packetize class for data
 for z in range(len(file_list)):
-	tgt = open(file_list[z],'rb')	#open each segment to variable 'tgt'
+	tgt = open("INPUT/"+file_list[z],'rb')	#open each segment to variable 'tgt'
 	pyld = tgt.read()					#actual pyld of pkt
 ##THIS SECTION IS FOR HEADER FIELD CREATION ATTACHMENT----------------------------------------------------------
 	pyld_len = len(pyld)				#get length of pyld in BYTES
@@ -63,19 +67,32 @@ for z in range(len(file_list)):
 ##END LEN HEADER CREATION		---------------------------------------------------------------------------		   
 	data = file_list[z] + len_header + pyld			#get orginal pyld with prefix and length header added on top
 	tgt.close()							#close tgt file, now we have our seg. pyld
-	new = packetize()					#create packetize class for data
 	segment = new.send(data)			#add header on top of data variable, store to segment variable
 	final = final + segment				#'stitch' together segments to final pyld
 	BER_CHECK += data					#Create a duplicate of expected output for BER testing without grc header
 	#if (z==1):							#IADDED THIS SO ONLY 2PKTS WENT THROUGH (BER TESTING)
 	#	break
 	
-GRC_input = open('TX_INPUT.bin','wb')	#create bin file that grc_tx will use as source
+###final needs 'trash pkts in case of gnu-radio syn, this means making a pkt9999 with a length of 100bytes of 'A', note it will need header stuff as well (trash-premable-length NO 2ndary header) [LISA protocol]
+dummy_pkt_len = 100
+dummy_pkt_data = ''
+dummy_pyld = 'AAAAAAAAAAAA'
+new = packetize(preamble = preamble)
+dummy_pkt_data = 'pkt9999'					#first add dummy filename
+dummy_pkt_data = dummy_pkt_data + dummy_pyld		#create one pyld of dummypkt with just pkt name
+segment = new.send(dummy_pkt_data)			#add first level header to segment variable with dummy pkt to be sent thourgh gnuradio
+final = segment + segment + segment + final + segment + segment + segment 		#ADDD 3 dummy pkts befoe and after relevant data
+##final now has pyld mized in with dummy pkts, we will write this data into the binary file that will be inputted into TX_grc
+
+
+
+GRC_input = open('TX_INPUT.bin','wb')	#create bin file that grc_tx will use as source OUTPUT of FILE
 GRC_input.write(final)					#add all segments with headers into file
 GRC_input.close()						#close file
-BER_sample = open('BER_samp.bin','wb')	#create binary file that will act as a check for BER in output bin file
-BER_sample.write(BER_CHECK)
-BER_sample.close()
+#BER_sample = open('BER_samp.bin','wb')	#create binary file that will act as a check for BER in output bin file OUTPUT OF FILE
+#BER_sample.write(BER_CHECK)
+#BER_sample.close()
+#delete("INPUT") # Delete the input folder after we're done.
 final = time.clock()
 
 print "Time Elapsed for Program:",final - intial				#TIMESTAMP for lines 1 -79
