23f57dd1443bca87723ba7081f26ff604ae9d08b
William Lahann (K_Laptop) <wlahann@gmu.edu>
2018-03-24 15:58:56 -0400

    Added unpack function, to seperate received packets into anticipated files.
    unpack unfction added to packetize library.

--- packetize.py
+++ packetize.py
@@ -61,3 +61,63 @@ class packetize():
         ### close socket on python's side ###
         self.sock.close()
         self.sock.shutdown()
+
+# Used in unpack to search through strings to find the index of the start of substrings
+def strSearch(mainString,searchPhrase):
+    indexs = [m.start() for m in re.finditer(searchPhrase,mainString)]
+    return indexs
+
+def unpack(GRCOutput,pktPrefix,outputLocation):
+
+    # This code was writen where the file location would not be specified with a forward slash at the end.
+    # This if statement removes the forward slash if it was included.
+    if outputLocation[len(outputLocation-1)] == "/":
+        outputLocation = outputLocation[:len(outputLocation-2)]
+
+    if len(split(prefix,"/")) > 1:
+        raise ValueError("prefix should not be a file path!")
+
+    # Setup the save location for the file segments
+    if os.path.exists(outputLocation):  # If the folder already exists, tag the time on the back so no data is deleted.
+        date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+        outputLocation = outputLocation+"-"+date
+        print("Files saved in: " + outputLocation)
+    outputLocation = outputLocation + "/"  # Add the forward slash for the file path.
+    os.makedirs(outputLocation)
+
+    # Load the output from the GRC file in as a string.
+    rxFile = open(GRCOutput,'rb')
+    rx = rxFile.read()
+    rxFile.close()
+
+    # Find the indexes in the GRC output file where the starts of the packets are found.
+    pktStarts = strSearch(rx,pktPrefix)
+
+    # Parse each packet that was received
+    for i in range(len(pktStarts)):
+        ind = pktStarts[i]  # This is the first index where the packet starts in the GRC file
+        name = rx[ind:ind+7]  # Get the name of the file from the packet.
+        if name != filePrefix+str(9999):  # packets 9999 are used for syncronization and not saved.
+            # Determine the length of the received file.
+            length = 0
+            for j in range(4):
+                temp = ord(rx[ind+7+j])
+                temp = temp * int(np.power(10,(3-j)))
+                length += temp
+            # Grab the from the body of the packet determined based on the length
+            data = rx[ind+10:ind+10+length]
+            if debug >= 1:
+                print("\nname: "+name)
+                print("length: "+str(length))
+                print("Data Length: "+str(len(data)))
+                print("data: "+data)
+
+            # Save the packet to the expected location. After this it should be set to use fileComb
+            pktSegment = open(outputLocation+name,'wb')
+            pktSegment.write(data)
+            pktSegment.close()
+        else:
+            print("Sync packet. Don't save.")
+            
+    # Return the output location, since it might not match what was asked for.
+    return outputLocation
