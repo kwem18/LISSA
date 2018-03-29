9f0c83f33b8b59c829d0243aa59cfa6993bc1177
kwem18 <37224053+kwem18@users.noreply.github.com>
2018-03-29 15:00:29 -0400

    Update GPIO_func
    Turned file into a class oriented program. Later will implement threading of some sort to allow 'sequential logic' GPIO control signal implementation.

--- GPIO_function.py
+++ GPIO_function.py
@@ -1,70 +1,69 @@
-###ERICK TERRAZAS 3/28/14###
+
+#------------------------------------------------------------------------------
+###MADE BY:ERICK TERRAZAS 3/28/14###
 ###ECE 493 Senior Design: Project LISSA
 ##/////////////////////GPIO_func.py////////////////////////////////////////////////
 
 # Objectives of file:
 # 1.Drive high voltage for enable of FEM using GPIO pin of adafruit FT232h board
 # 2.While Enable is driven high, another GPIO pin should be driven low for Rx
-# 3. While enable is driven high, another GPIO pin should be driven hihg for Tx
+# 3. While enable is driven high, another GPIO pin should be driven high for Tx
 # 4. Initialize USB PORT CONNECTION
 
 ### MODULES------------------------------------------------------------------------
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.FT232H as FT232H
-import time # For time stamp
+import time # For time
 
 
 # END MODULES----------------------------------------------------------------------
 
 ###DEFINITIONS---------------------------------------------------------------------
+class GPIO_function:
 
-def INIT_FEM():
-    print 'INITIALIZE GPIO BOARD...'
-    FT232H.use_FT232H()         #Temorarily disable FTDI Driver
-    ft232h = FT232H.FT232H()    #Find device
-    ft232h.setup(8,GPIO.OUT)    #PIN 8 maps to BOARD PORT C0    CONTROL SIGNAL
-    ft232h.setup(9,GPIO.OUT)   #PINT 9 maps to BOARD PORT C1   ENABLE SIGNAL
-    return
+    def __init__(self):
+        print 'INITIALIZE GPIO BOARD'
+        FT232H.use_FT232H()             #Temorarily disable FTDI Driver
+        self.ft232h = FT232H.FT232H()    #Find device
+        self.ft232h.setup(8,GPIO.OUT)    #PIN 8 maps to BOARD PORT C0    CONTROL SIGNAL
+        self.ft232h.setup(9,GPIO.OUT)   #PINT 9 maps to BOARD PORT C1   ENABLE SIGNAL
+        self.initial = int(0)
+        self.final = int(0)
 
 
-def ENABLE_FEM(switch):
-    initial = time.clock()  #Start clocking in time displacement
-    #FT232H.use_FT232H() #Temorarily dsiable FTDI Driver
-    #ft232h = FT232H.FT232H()    #Find device
-    #ft232h.setup(8,GPIO.OUT)    #PIN 8 maps to BOARD PORT C0    CONTROL SIGNAL
-    #ft232h.sestup(9,GPIO.OUT)   #PINT 9 maps to BOARD PORT C1   ENABLE SIGNAL
-    if (switch == 0):
-        print '\nFEM SHUTTING DOWN...'
-        ft232h.output(9,GPIO.LOW)       #TURN ENABLE SIGNAL OFF
-        time,sleep(0.4)                 #Cautionary pause to avoid data overload of GPIO board
-        ft232h.output(8,GPIO.LOW)       #Drive Control signal to low voltage
-        final = time.clock()
-        print "Time elapsed:",final-initial
+    def ENABLE_FEM(self,switch):
+        initial = time.clock()  #Start clocking in time displacement
+        if (switch == 0):
+            print '\nFEM SHUTTING DOWN...'
+            self.ft232h.output(9,GPIO.LOW)       #TURN ENABLE SIGNAL OFF
+            time.sleep(0.4)                 #Cautionary pause to avoid data overload of GPIO board
+            self.ft232h.output(8,GPIO.LOW)       #Drive Control signal to low voltage
+            self.final = time.clock()
+            print "Time elapsed:",(self.final- self.initial)
 
-    elif (switch == 1):
-        print '\nFEM ACTIVATING...'
-        ft232h.output(9,GPIO.HIGH)      #TURN ENABLE SIGNAL ON
-        time.sleep(0.4)
-        print
-        "Time elapsed:", final - initial
+        elif (switch == 1):
+            print '\nFEM ACTIVATING...'
+            self.ft232h.output(9,GPIO.HIGH)      #TURN ENABLE SIGNAL ON
+            time.sleep(0.4)
+            print "Time elapsed:",(self.final - self.initial)
 
-    else:
-        raise ValueError('MUST INPUT integer 1 (ON) or integer 0 (OFF)')
+        else:
+            raise ValueError('MUST INPUT integer 1 (ON) or integer 0 (OFF)')
+            return
+
         return
 
-    return
 
+    def TX_FEM(self):
+        print '\nFEM ----> TX MODE'
+        self.ft232h.output(8,GPIO.HIGH)          #DRIVE CONTROL SIGNAL TO HIGH VOLTAGE
+        time.sleep(0.4)
+        return
 
-def TX_FEM():
-    print '\nFEM ----> TX MODE'
-    ft232h.output(8,GPIO.HIGH)          #DRIVE CONTROL SIGNAL TO HIGH VOLTAGE
-    time.sleep(0.4)
-    return
-
-def RX_FEM():
-    print '\nFEM ----> RX MODE'
-    ft232h.output(8,GPIO.LOW)           #DRIVE CONTROL SIGNAL TO LOW VOLTAGE
-    time.sleep(0.4)
-    return
+    def RX_FEM(self):
+        print '\nFEM ----> RX MODE'
+        self.ft232h.output(8,GPIO.LOW)           #DRIVE CONTROL SIGNAL TO LOW VOLTAGE
+        time.sleep(0.4)
+        return
 
 
